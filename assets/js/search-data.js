52feeee647ec8cb99ba77f18107d9257983589ff
shashank-signalling <shashank.srivastava90@gmail.com>
2026-04-01 08:32:02 +0000

    Deploying to gh-pages from @ shashank-signalling/shashank-signalling.github.io@d5c5fdc29e5d22046bf6dac1999dbf07780e86b2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
+        },{id: "nav-research",
+          title: "Research",
           description: "My research focuses on how intracellular signaling networks regulate immune cell function in health and disease. In particular, I investigate how signaling pathway crosstalk shapes the activation, differentiation, and functional plasticity of lymphocytes within complex tissue environments. Dysregulation of these pathways is a key driver of immune-mediated diseases, including cancer and chronic inflammation. Understanding how these signaling networks integrate at the cellular level provides a framework for identifying new therapeutic strategies.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/Projects/";
+            window.location.href = "/Research/";
           },
         },{id: "nav-repositories",
           title: "repositories",
